1cc8e36a62e465f55a222c4ddba4190ef742de4b
GedeonRO <otakuagbolossou@gmail.com>
2024-10-18 14:05:55 +0000

    ....

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import Apropos from "./components/About";
+import Apropos from "./components/about";
 import Presentations from "./components/Presentation";
 import Navbar from "./components/Navbar";
-import Competences from "./components/Skills";
-import Experiences from "./components/Experience";
+import Competences from "./components/skills";
+import Experiences from "./components/experience";
 import Works from "./components/Work";
 import ContactMe from "./components/contact";
 import { ToastContainer } from "react-toastify";
